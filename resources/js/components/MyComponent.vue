e9cae0888dd65628f793f959920734bc28a32b72
sejal0610 <60104803+sejal0610@users.noreply.github.com>
2020-03-29 19:02:32 +0530

    v0.1 - Made Layouts

---
+++ resources/js/components/MyComponent.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-container fluid fill-height>
+    <v-layout align-center justify-center>
+      <v-btn color="primary" @click="setLayout('frontend-layout')">Frontend Layout</v-btn>
+      <v-btn color="secondary" @click="setLayout('backend-layout')">Backend Layout</v-btn>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  methods: {
+    setLayout (layout) {
+      this.$store.commit('SET_LAYOUT', layout)
+    }
+  }
+}
+</script>
