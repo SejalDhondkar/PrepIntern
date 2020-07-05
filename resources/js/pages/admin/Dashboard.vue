a08261d8e0cf69d8e055534ac60af2956d874fd2
Sejal Dhondkar <sejald555@gmail.com>
2020-07-05 13:29:17 +0530

    Admin Dashboard

--- resources/js/pages/admin/Dashboard.vue
+++ resources/js/pages/admin/Dashboard.vue
@@ -11,14 +11,14 @@
       dark
     >
     <v-card-text>
-      <v-toolbar-title>Dashboard</v-toolbar-title>
+      <v-toolbar-title>Dashboard (List of Users)</v-toolbar-title>
       
     </v-card-text>
     </v-toolbar>
           
         <v-container class="p-4">
  
-          <v-radio-group v-model="users.role_id" row @change="status()">
+          <v-radio-group v-model="group.role_id" row @change="status()">
                 <v-radio label="All Users" value="0"></v-radio>
                 <v-radio label="Students" value="5"></v-radio>
                 <v-radio label="Companies" value="4"></v-radio>
@@ -60,6 +60,9 @@ export default {
           
         ],
         users: [],
+        group: {
+          role_id: "0",
+        }
         
       }
     },
@@ -68,16 +71,13 @@ export default {
       this.$store.commit('SET_LAYOUT', 'admin-layout');
 			axios.get('/admin/users/list').then(response => {
             this.users = response.data;
-            console.log(this.users);
       });
     },
 
     methods:{
       status(){
-        axios.get('/admin/users/sort',{params: {status: this.users.role_id}}).then(response => {
-       
+        axios.get('/admin/users/sort',{params: {status: this.group.role_id}}).then(response => {       
             this.users = response.data;
-             console.log(this.users);   
       });
       },
     } 
