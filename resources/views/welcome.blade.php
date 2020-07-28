2d4693dff658caf8a455b163885ce53e93ffb991
Shubh Jangam <shubhjangam100@gmail.com>
2020-07-28 19:35:58 +0530

    Home Page Fixed

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'PrepIntern') }}</title>
+    <title>PrepIntern</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
